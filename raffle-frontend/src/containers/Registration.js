019fdf1b49db2f2acb9801a20e3339c54c2fbc77
antonzaharia <zaharia_anton@yahoo.com>
2020-10-15 19:34:55 +0100

    registration show login fixed

--- raffle-frontend/src/containers/Registration.js
+++ raffle-frontend/src/containers/Registration.js
@@ -1,45 +1,57 @@
-import React, { Component } from 'react';
-import SignupForm from '../components/SignupForm';
-import LoginForm from '../components/LoginForm';
-import Button from 'react-bootstrap/Button'
+import React, { Component } from "react";
+import SignupForm from "../components/SignupForm";
+import LoginForm from "../components/LoginForm";
+import Button from "react-bootstrap/Button";
 
 export default class Registration extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            loginForm: false,
-            signupForm: false
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      loginForm: "Login",
+      signupForm: "Signup",
+    };
+  }
+
+  loadForm = () => {
+    if (this.state.loginForm === "X") {
+      return <LoginForm />;
+    } else if (this.state.signupForm === "X") {
+      return <SignupForm />;
     }
-    
-    loadForm = () => {
-        if(this.state.loginForm){
-            return <LoginForm />
-        } else if(this.state.signupForm) {
-            return <SignupForm />
-        }
+  };
+  showLogin = () => {
+    if (this.state.loginForm === "Login") {
+      this.setState({
+        loginForm: "X",
+        signupForm: "Signup",
+      });
+    } else {
+      this.setState({
+        loginForm: "Login",
+        signupForm: "Signup",
+      });
     }
-    showLogin = (event) => {
+  };
+  showSignup = () => {
+    if (this.state.signupForm === "Signup") {
         this.setState({
-            loginForm: !this.state.loginForm,
-            signupForm: false
-        })
-        this.state.loginForm ? event.target.textContent = "Login" : event.target.textContent = "X" 
-    }
-    showSignup = (event) => {
+          loginForm: "Login",
+          signupForm: "X",
+        });
+      } else {
         this.setState({
-            loginForm: false,
-            signupForm: !this.state.signupForm
-        })
-        this.state.signupForm ? event.target.textContent = "Signup" : event.target.textContent = "X" 
-    }
-    render() {
-        return (
-            <div className="right-text">
-                <Button onClick={this.showLogin}>Login</Button>
-                <Button onClick={this.showSignup}>Signup</Button>
-                {this.loadForm()}
-            </div>
-        )
-    }
+          loginForm: "Login",
+          signupForm: "Signup",
+        });
+      }
+  };
+  render() {
+    return (
+      <div className="right-text">
+        <Button onClick={this.showLogin}>{this.state.loginForm}</Button>
+        <Button onClick={this.showSignup}>{this.state.signupForm}</Button>
+        {this.loadForm()}
+      </div>
+    );
+  }
 }
