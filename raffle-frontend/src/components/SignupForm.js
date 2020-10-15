0111d3e293ff43a5915e0616e789fa997ab1c418
antonzaharia <zaharia_anton@yahoo.com>
2020-10-15 19:08:23 +0100

    signup form style finished

--- raffle-frontend/src/components/SignupForm.js
+++ raffle-frontend/src/components/SignupForm.js
@@ -1,25 +1,34 @@
 import React, { Component } from "react";
+
+// Bootstrap
 import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faArrowRight } from '@fortawesome/free-solid-svg-icons'
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+
+// Fontawesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
 
 export default class signupForm extends Component {
   render() {
     return (
       <div>
-        <Form>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Control type="text" placeholder="Enter your name" />
-          </Form.Group>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Control type="email" placeholder="Enter your email" />
-          </Form.Group>
-          <Form.Group controlId="formBasicPassword">
-            <Form.Control type="password" placeholder="Enter your password" />
-          </Form.Group>
-          <Button>Done <FontAwesomeIcon icon={faArrowRight}/></Button>
-        </Form>
+        <Card style={{ width: '18rem'}} className="float-right">
+          <Form>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control type="text" placeholder="Enter your name" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control type="email" placeholder="Enter your email" />
+            </Form.Group>
+            <Form.Group controlId="formBasicPassword">
+              <Form.Control type="password" placeholder="Enter your password" />
+            </Form.Group>
+            <Button type="submit" className="center-text">
+              Done <FontAwesomeIcon icon={faArrowRight} />
+            </Button>
+          </Form>
+        </Card>
       </div>
     );
   }
