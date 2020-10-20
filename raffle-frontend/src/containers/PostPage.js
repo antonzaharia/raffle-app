f2f69975ef11a92359d0bdb60ee849cf05864c4d
antonzaharia <zaharia_anton@yahoo.com>
2020-10-20 20:54:37 +0100

    post page form

--- raffle-frontend/src/containers/PostPage.js
+++ raffle-frontend/src/containers/PostPage.js
@@ -15,7 +15,6 @@ class PostPage extends Component {
     if (this.props.post) {
       return (
         <div className="main">
-            {console.log(this.props.post)}
           <Card>
             <Card.Img variant="top" src={this.props.post.image} />
           </Card>
@@ -25,7 +24,27 @@ class PostPage extends Component {
               <Card.Title>{this.props.post.title}</Card.Title>
               <Card.Text>{this.props.post.description}</Card.Text>
               <Card.Title>$ {this.props.post.price} per ticket</Card.Title>
+              <hr />
+              <Card.Title>You want to get in?</Card.Title>
+              <Card.Text>Answer the following question.</Card.Text>
               <Form>
+                <Card.Header>
+                  <h4>{this.props.post.question.content}</h4>
+                </Card.Header>
+                <div className="answers-flex">
+                {this.props.post.answers[0].map((answer) => (
+                  <Form.Check
+                    custom
+                    className="answer"
+                    name="answer"
+                    key={answer.id}
+                    id={answer.id}
+                    type="radio"
+                    label={answer.content}
+                  />
+                ))}
+                </div>
+                <Form.Control type="number" value="1" min="1" max="30"/>
                 <Button variant="primary">Go somewhere</Button>
               </Form>
             </Card.Body>
