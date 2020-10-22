700098f3c51559d3e0c8cc8a181ec7cabefca2c3
antonzaharia <zaharia_anton@yahoo.com>
2020-10-22 22:10:43 +0100

    user serializer modified for signup

--- raffle-backend/app/serializers/user_serializer.rb
+++ raffle-backend/app/serializers/user_serializer.rb
@@ -2,6 +2,9 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :cart_id
 
   def cart_id
-    cart_id = object.carts.last.id
+    cart_id = false
+    if object.carts.size > 0
+      cart_it = object.carts.last.id
+    end
   end
 end
