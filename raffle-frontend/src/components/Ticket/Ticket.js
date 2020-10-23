1f5c43aafd95e40eb0d653b943e31a49bfdf5291
antonzaharia <zaharia_anton@yahoo.com>
2020-10-23 20:53:32 +0100

    ticket component finished

--- raffle-frontend/src/components/Ticket/Ticket.js
+++ raffle-frontend/src/components/Ticket/Ticket.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from "react";
 
-export default function Ticket() {
-    return (
-        <div>
-            
-        </div>
-    )
+import Card from "react-bootstrap/Card";
+import { Link } from "react-router-dom";
+
+export default function Ticket({ ticket }) {
+  const makeLink = (id) => {
+    return `/posts/${id}`;
+  };
+  return (
+    <div>
+      <Card>
+        <Card.Body>
+          <span>Ticket number: {ticket.number}</span>
+          <span className="float-right">
+            See post: <Link to={makeLink(ticket.post_id)}>Post</Link>
+          </span>
+        </Card.Body>
+      </Card>
+    </div>
+  );
 }
--- raffle-frontend/src/containers/TicketsContainer.js
+++ raffle-frontend/src/containers/TicketsContainer.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import Ticket from '../components/Ticket/Ticket'
 
 export default function TicketsContainer({tickets}) {
     return (
         <div>
+            <h3 className="center-text">Your Tickets:</h3>
             {tickets.map( ticket => {
-                    return <p>{ticket.number}</p>
+                    return <Ticket key={ticket.id} ticket={ticket} />
             })}
         </div>
     )
