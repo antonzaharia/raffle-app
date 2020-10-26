1974b43615dc71a2d5cc1153de235ac43b9a035f
antonzaharia <zaharia_anton@yahoo.com>
2020-10-26 15:58:09 +0000

    tickets winning status styled

--- raffle-frontend/src/components/Ticket/Ticket.js
+++ raffle-frontend/src/components/Ticket/Ticket.js
@@ -1,15 +1,27 @@
 import React from "react";
-import { makeLink } from '../../helpers/helpers'
+import { makeLink } from "../../helpers/helpers";
 
 import Card from "react-bootstrap/Card";
 import { Link } from "react-router-dom";
+import Badge from "react-bootstrap/Badge"
 
 export default function Ticket({ ticket }) {
+  const showWon = () => {
+    if(ticket.winner === false) {
+      return <Badge variant="warning">Raffle still on pending</Badge>
+    } else if (ticket.winner === "none") {
+      return <Badge variant="secondary">Not a Winner</Badge>
+    } else {
+      return <h3><Badge variant="success">Won</Badge></h3> 
+    }
+  }
   return (
     <div>
       <Card>
         <Card.Body>
-          <span>Ticket number: {ticket.number}</span>
+          <span>
+            Ticket number: {ticket.number} || {showWon()}
+          </span>
           <span className="float-right">
             See post: <Link to={makeLink(ticket.post_id)}>Post</Link>
           </span>
