6b0c1a1f2c6b71fc33571325ab37943ae496d084
antonzaharia <zaharia_anton@yahoo.com>
2020-10-26 20:29:41 +0000

    home page check date modified

--- raffle-frontend/src/components/Raffle/DateCheck.js
+++ raffle-frontend/src/components/Raffle/DateCheck.js
@@ -3,9 +3,10 @@ import FeaturedPosts from './FeaturedPosts'
 
 export default function DateCheck({ posts }) {
     const today = new Date
+    const todayDate = `${today.getFullYear()}-${today.getMonth()+1}-${today.getDate()}`
     let featuredPosts = ""
     if (posts) {
-        featuredPosts = posts.filter( post => post.date === "2020-10-25")
+        featuredPosts = posts.filter( post => post.date === todayDate)
     }
     return (
         <div>
