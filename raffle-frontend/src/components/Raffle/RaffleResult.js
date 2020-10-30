4e1c8d6e7b1c7314a863587f3b8ad83f83f3edd3
antonzaharia <zaharia_anton@yahoo.com>
2020-10-30 18:16:15 +0000

    refactor

--- raffle-frontend/src/components/Raffle/RaffleResult.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function RaffleResult() {
-    return (
-        <div>
-            Result
-        </div>
-    )
-}
--- raffle-frontend/src/containers/Admin/AdminPostCard.js
+++ raffle-frontend/src/containers/Admin/AdminPostCard.js
@@ -6,15 +6,10 @@ import { Link } from "react-router-dom";
 import { makeLink } from "../../helpers/helpers";
 import { connect } from 'react-redux'
 import { deletePost, rafflePost } from "../../actions/PostsActions";
-import RaffleResult from "../../components/Raffle/RaffleResult";
 
 function AdminPostCard({ post, deletePost, rafflePost }) {
   const makeEditLink = (id) => {
     return `/posts/${id}/edit`
-  }
-  const handleRaffle = () => {
-    rafflePost(post.id)
-    return <RaffleResult />
   }
   return (
     
@@ -23,12 +18,11 @@ function AdminPostCard({ post, deletePost, rafflePost }) {
       || {post.tickets_number} tickets || &nbsp;&nbsp;
       {post.winner ? <span>Winning Ticket: <Badge variant="primary">{post.winner}</Badge> ||&nbsp;&nbsp;</span> : <span>Raffle on: || {post.date} ||&nbsp;&nbsp;</span>}
       <Link to={makeEditLink(post.id)}><Button variant="warning">Edit</Button>&nbsp;</Link>
-      <Button onClick={handleRaffle} variant="success" >Raffle</Button>&nbsp;
+      <Button onClick={() => rafflePost(post.id)} variant="success" disabled={post.winner ? "disabled" : ""}>Raffle</Button>&nbsp;
       <Button onClick={() => deletePost(post.id)} variant ="danger">Delete</Button>
     </Card>
   );
 }
-// disabled={post.winner ? "disabled" : ""}
 const mapDispatchToProps = dispatch => ({
   deletePost: id => dispatch(deletePost(id)),
   rafflePost: id => dispatch(rafflePost(id))
