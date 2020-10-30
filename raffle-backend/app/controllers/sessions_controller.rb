e15168d7aef1d65088a547fdfaebbc6e3decc822
antonzaharia <zaharia_anton@yahoo.com>
2020-10-30 20:45:19 +0000

    status

--- raffle-backend/app/controllers/sessions_controller.rb
+++ raffle-backend/app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
     def create
         user = User.find_by(email: params[:email])
         if user && user.authenticate(params[:password])
-            render json: user
+            render json: user, status: :ok
         else
-            render json: {errors: {Invalid: "Email or Password"}}
+            render json: {errors: {Invalid: "Email or Password"}, status: :unauthorized}
         end
     end
 end
--- raffle-backend/app/controllers/users_controller.rb
+++ raffle-backend/app/controllers/users_controller.rb
@@ -2,9 +2,9 @@ class UsersController < ApplicationController
     def create
         user = User.new(name: params[:name], email: params[:email], password: params[:password])
         if user.save
-            render json: UserSerializer.new(user).to_json
+            render json: UserSerializer.new(user).to_json, status: :ok
         else
-            render json: {errors: user.errors}
+            render json: {errors: user.errors, status: :unauthorized}
         end
     end
 
