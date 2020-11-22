3ea15c054ce9520a2bb19c1ea656e18435eb0299
antonzaharia <zaharia_anton@yahoo.com>
2020-11-22 13:00:30 +0000

    update to postgres

--- raffle-backend/db/schema.rb
+++ raffle-backend/db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_10_24_175255) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "answers", force: :cascade do |t|
     t.string "content"
     t.integer "question_id"
